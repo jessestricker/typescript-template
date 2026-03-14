8196d7b5751513966312b9228ebdddbb5cf4ff8e
Jesse Stricker <git@jessestricker.de>
2026-03-14 22:25:54 +0100

    chore: setup testing

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+import { add } from "../src/index.ts";
+
+describe("add", () => {
+  it("should return the sum", () => {
+    assert.equal(add(1, 2), 3);
+  });
+});
